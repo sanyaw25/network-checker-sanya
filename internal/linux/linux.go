fa2d243a4af470a076656f6690dc6f126bed77cc
InfectedMunshroom <h.a.ayushman@gmail.com>
2024-09-29 02:36:59 +0530

    Code refactoring, adding credits
    Signed-off-by: InfectedMunshroom <h.a.ayushman@gmail.com>
    
    Changes to be committed:
    	modified:   internal/linux/linux.go
    	modified:   internal/windows/windows.go
    	modified:   main.go

--- internal/linux/linux.go
+++ internal/linux/linux.go
@@ -1,3 +1,4 @@
+// By @InfectedMunshroom
 package linux
 
 import (
--- internal/windows/windows.go
+++ internal/windows/windows.go
@@ -1,3 +1,4 @@
+// By @sanyaw25
 package windows
 
 import (
@@ -7,7 +8,6 @@ import (
 	"regexp"
 )
 
-// GetHostIPAndMAC retrieves the first non-loopback IP address and MAC address of the system
 func GetHostIPAndMAC() (string, string, error) {
 	interfaces, err := net.Interfaces()
 	if err != nil {
@@ -15,7 +15,6 @@ func GetHostIPAndMAC() (string, string, error) {
 	}
 
 	for _, iface := range interfaces {
-		// Skip loopback interfaces
 		if iface.Flags&net.FlagLoopback != 0 {
 			continue
 		}
@@ -34,7 +33,6 @@ func GetHostIPAndMAC() (string, string, error) {
 				ip = v.IP
 			}
 
-			// Check if it's a valid IPv4 address, skipping loopback
 			if ip != nil && !ip.IsLoopback() && ip.To4() != nil {
 				return ip.String(), iface.HardwareAddr.String(), nil
 			}
@@ -44,30 +42,25 @@ func GetHostIPAndMAC() (string, string, error) {
 	return "", "", fmt.Errorf("no valid IP address found")
 }
 
-// ExtractPingStats runs a ping to a given URL and extracts min/avg/max times and packet loss
 func ExtractPingStats(url string) (string, string, string, string, string, error) {
-	// Windows uses -n for the count of ping requests
 	cmd := exec.Command("ping", "-n", "5", url)
-	output, err := cmd.CombinedOutput() // Use CombinedOutput to get stderr and stdout
+	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return "", "", "", "", "", fmt.Errorf("error running ping command: %w", err)
 	}
 
 	pingOutput := string(output)
-	fmt.Println("Ping Command Output:\n", pingOutput) // Display raw output
+	fmt.Println("Ping Command Output:\n", pingOutput)
 
-	// Regular expression for extracting the IP address from the ping output
 	ipRe := regexp.MustCompile(`Pinging ([\w.-]+) \[(\d+\.\d+\.\d+\.\d+)\]`)
-	// Regular expression for extracting the min/avg/max ping times
 	statsRe := regexp.MustCompile(`Minimum = (\d+)ms, Maximum = (\d+)ms, Average = (\d+)ms`)
-	// Regular expression for extracting packet loss information
 	packetLossRe := regexp.MustCompile(`Lost = (\d+) \((\d+)% loss\)`)
 
 	ipMatch := ipRe.FindStringSubmatch(pingOutput)
 	if len(ipMatch) < 3 {
 		return "", "", "", "", "", fmt.Errorf("no IP address found")
 	}
-	ipAddress := ipMatch[2] // The IP address is in the second capturing group
+	ipAddress := ipMatch[2]
 
 	statsMatch := statsRe.FindStringSubmatch(pingOutput)
 	if len(statsMatch) < 4 {
@@ -85,30 +78,3 @@ func ExtractPingStats(url string) (string, string, string, string, string, error
 
 	return ipAddress, minTime, avgTime, maxTime, packetLoss, nil
 }
-
-// main function to test the functionalities
-// func main() {
-// ip, mac, err := GetHostIPAndMAC()
-// if err != nil {
-// fmt.Println("Error getting IP and MAC:", err)
-// return
-// }
-// fmt.Println("IP Address:", ip)
-// fmt.Println("MAC Address:", mac)
-//
-// Change the URL to a valid address for pinging
-// url := "google.com" // Example URL
-// ipAddress, minTime, avgTime, maxTime, packetLoss, err := ExtractPingStats(url)
-// if err != nil {
-// fmt.Println("Error extracting ping stats:", err)
-// return
-// }
-//
-// fmt.Printf("Ping Statistics for %s:\n", url)
-// fmt.Printf("IP Address: %s\n", ipAddress)
-// fmt.Printf("Minimum Time: %s ms\n", minTime)
-// fmt.Printf("Average Time: %s ms\n", avgTime)
-// fmt.Printf("Maximum Time: %s ms\n", maxTime)
-// fmt.Printf("Packet Loss: %s%%\n", packetLoss)
-// }
-//
